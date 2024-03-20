6c38ade4a7fd7c2e98a3c13bbc669d93bb294f27
aizazulllhaq <aizazulllhaq@gmail.com>
2024-03-20 14:52:54 +0500

    fix - Remove useless comments

--- src/controllers/Home.Controller.js
+++ src/controllers/Home.Controller.js
@@ -23,48 +23,37 @@ const tapMining = wrapAsync(async (req, res, next) => {
 
     // change mining status to false , after 12 hours
     setTimeout(async () => {
-        let incrementPointLevel = 1.03; 
+
+        let incrementPointLevel = 1.03;
+
         // Direct Referred Increment
         const user = await User.findById(req.user?.id);
         if (user) {
             if (user.directReferred.length > 0) {
-                // console.log(user.directReferred)
 
                 // Interating Direct Referred Users
                 // NOTE !! map execute asyncronously , to wait for all the iteration we need to use ( await Promise.all )
                 await Promise.all(user.directReferred.map(async (directReferredUser) => { 
-                    // console.log("direct user interation ", directReferredUser)
                     const verifyDirectReferredUser = await User.findOne({ referredCode: directReferredUser });
                     console.log("verifyDirectUser : ", verifyDirectReferredUser)
                     if (verifyDirectReferredUser.is_verified) {
                         incrementPointLevel += incrementPointLevel * 1.03;
-                        // console.log("increment point level direct referred user : ", incrementPointLevel)
                     }
                 }))
-            }
-            else {
-                console.log("No Direct-Referred Found");
             }
 
             // Indirect Referred Increment
             if (user.indirectReffered.length > 0) {
-                // console.log(user.indirectReffered);
 
                 // Interating Indirect Referred Users
                 await Promise.all(user.indirectReffered.map(async (indirectReferredUser) => {
-                    // console.log("indirect user interation", indirectReferredUser);
-
                     const verifyIndirectReferrerdUser = await User.findOne({ referredCode: indirectReferredUser });
-                    console.log("verifyIndirectReferrerdUser : ", verifyIndirectReferrerdUser)
                     if (verifyIndirectReferrerdUser.is_verified) {
                         incrementPointLevel += incrementPointLevel * 0.7
-                        // console.log("increment point level indirect referred user : ", incrementPointLevel)
                     }
                 }))
             }
         }
-
-        console.log("globally", incrementPointLevel)
 
         await User.findByIdAndUpdate(
             { _id: req.user?.id },
@@ -78,7 +67,7 @@ const tapMining = wrapAsync(async (req, res, next) => {
                 }
             });
         console.log("Mining Status Reset after 12 hours");
-    }, 10000) // 12 * 60 * 60 * 1000 = 12 hours &  60 * 1000 = 1 minute 
+    }, 12 * 60 * 60 * 1000) // 12 * 60 * 60 * 1000 = 12 hours &  60 * 1000 = 1 minute 
 
     // return response with updatedUser
     return res
